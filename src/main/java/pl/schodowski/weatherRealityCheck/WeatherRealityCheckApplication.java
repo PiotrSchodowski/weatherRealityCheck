395bcbda1c576bc36b836f9dbc197bd07c3d602d
Piotr Schodowski <ps.schodowski@gmail.com>
2024-01-31 23:37:36 +0100

    first commit

---
+++ src/main/java/pl/schodowski/weatherRealityCheck/WeatherRealityCheckApplication.java
@@ -0,0 +1,13 @@
+package pl.schodowski.weatherRealityCheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherRealityCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherRealityCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/schodowski/weatherRealityCheck/WeatherRealityCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.schodowski.weatherRealityCheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherRealityCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
