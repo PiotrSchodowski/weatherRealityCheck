5c8c504997ad0dfaecb03580a3f32d5791215192
Piotr Schodowski <ps.schodowski@gmail.com>
2024-02-26 18:10:48 +0100

    made class DifferenceForMeasurementsEntity and refactor Compare service, count percentage

--- src/main/java/pl/schodowski/weatherRealityCheck/repository/DiffForMeasurementsRepository.java
+++ src/main/java/pl/schodowski/weatherRealityCheck/repository/DiffForMeasurementsRepository.java
@@ -1,7 +1,9 @@
 package pl.schodowski.weatherRealityCheck.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.schodowski.weatherRealityCheck.entity.DifferenceForMeasurementsEntity;
 
+@Repository
 public interface DiffForMeasurementsRepository extends JpaRepository<DifferenceForMeasurementsEntity, Long> {
 }
